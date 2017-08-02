bab7ab25d14b3b996ccb8f4d20005780c99b4e2a
Exarchiasghost <exarchiasghost@gmail.com>
2017-08-02 02:54:57 +0200

    Challenge 1 is done!

---
+++ Java essential training for students/c1/MyJava/Challenge1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Challenge1 {
+	public static void main(String[] args){
+	System.out.println("Enter your username: ");
+	Scanner scanner = new Scanner(System.in);
+	String username = scanner.nextLine();
+	System.out.println("Your username is " + username);
+	}
+}
